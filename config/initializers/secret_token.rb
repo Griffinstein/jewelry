22e1044c153ab1f40dccc8d52f022da5b8f03ba7
Edward <eddmccaffrey@gmail.com>
2013-11-24 05:13:37 +1100

    Uploading full app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jewelry::Application.config.secret_key_base = 'a8c06a426e055f77b96afb5aa6d252b58eabe9e35c9f75970e143e5817f3d058c9fff27a2a222437fdfc503b4b42b47d4faf910f3c1bbc1c8d5456d98601534c'
